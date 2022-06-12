ba7837b77f2653462976385318e2cd162038fd76
BahaaNimer <bahaanimer97@gmail.com>
2022-06-12 18:58:28 +0300

    build a server with auth

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -18,22 +18,7 @@ describe('web server', () => {
     })
     expect(response.status).toBe(201);
   });
-  it('it should login as a user', async () => {
-    const response = await request.post('/signin').send({
-      "username": "test",
-      "password": "test"
-    })
-    expect(response.status).toBe(200);
-  });
-  it('it should not login as a user', async () => {
-    const response = await request.post('/signin').send({
-      "username": "test",
-      "password": "test1"
-    })
-    expect(response.status).toBe(403);
-  });
 });
-
 
 afterAll(async () => {
   await db.drop();
